8d3ecb8a074bfc91cdd05b4e0862ef5bce455c16
chiefautoparts <chiefautoparts@outlook.com>
2018-07-06 01:34:51 -0700

    background color changes on gendata page

--- apps/team/urls.py
+++ apps/team/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	url(r"^team_gen$", views.gen, name="gen"),
 	url(r"^gen_data/", views.gen_data, name="gen_data"),
-	url(r"^assignPlayers$", views.assignPlayers, name="assignPlayers")
+	url(r"^assignPlayers$", views.assignPlayers, name="assignPlayers"),
+	# url(r"^)
 ]
